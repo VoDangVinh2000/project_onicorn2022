6253fb70d23f4c678038a487b3e1c8aa59bf3d4c
trinhduyen1505 <trinhthimyduyen1505@gmail.com>
2022-02-24 08:06:28 +0700

    add banner

---
+++ onicorn2022_project/app/Models/Banner/Banner.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Banner;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Banner extends Model
+{
+    use HasFactory;
+    protected $table = "la_banner";
+    
+    protected $fillable = [
+        'page_id',
+        'title',
+        'subtitle',
+        'photo',
+        'enabled',
+        
+    ];
+    // public function page()
+    // {
+    //     return $this->belongsTo('App\Models\Page\Page','page_id','id');
+    // }
+}
